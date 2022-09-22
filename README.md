43d15874d66655644cc3d9602d8f1232f7c99e7b
KristofferC <kristoffer.carlsson@juliacomputing.com>
2022-09-22 13:34:21 +0200

    add support for artifacts

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # PkgFsck.jl
 
-PkgFsck.jl is a package to find corrupted packages in depots and fix them.
+PkgFsck.jl is a package to find corrupted packages and artifacts in depots and fix them.
+
+## Packages
 
 To show how it works we download a package and corrupt it:
 
@@ -17,27 +19,31 @@ julia> touch(joinpath(dirname(example_path), "corruption"))
 ```julia
 julia> import PkgFsck
 
-julia> failures = PkgFsck.fsck();
+julia> failures = PkgFsck.fsck_packages();
 
 julia> filter!(x->!x.has_build_file, failures) # See Caveats below
-1-element Vector{PkgFsck.FsckFailure}:
- PkgFsck.FsckFailure("/home/kc/.julia/packages/Example/aqsx3", UUID("7876af07-990d-54b4-ab0e-23690620f79a"), SHA1("46e44e869b4d90b96bd8ed1fdcf32244fddfb6cc"), false)
+1-element Vector{PkgFsck.PkgFsckFailure}:
+ PkgFsck.PkgFsckFailure("/home/kc/.julia/packages/Example/aqsx3", UUID("7876af07-990d-54b4-ab0e-23690620f79a"), SHA1("46e44e869b4d90b96bd8ed1fdcf32244fddfb6cc"), false)
 
 julia> foreach(PkgFsck.redownload, failures) # Removes the corrupted folder and redownloads the package
 ```
 
-## Caveats
+### Caveats
 
-### Build
+#### Build
 
 Packages using the `deps/build.jl` file often give false positives since they
 tend to create files into the package directory and there is no way
 to know which files are part of the original download. These packages
 have the `has_build_file` property set to `true` in the returned
-bjects of `fsck` allowing them to be filtered out.
+objects of `fsck_packages` allowing them to be filtered out.
 
-### Coverage files
+#### Coverage files
 
 The coverage system in Julia tend to put `.cov` file into the package
 directories. By default, `fsck` will try to clean these out, this can be set
 with the `remove_cov_files::Bool` keyword to `fsck`.
+
+## Artifacts
+
+Detecting and fixing corrupted artifacts work in the same way. Run `fsck_artifacts()` and then `redownload` on the items in the returned vector.
--- src/PkgFsck.jl
+++ src/PkgFsck.jl
@@ -1,6 +1,6 @@
 module PkgFsck
 
-export fsck, redownload
+export fsck_packages, redownload, fsck_artifacts
 
 using Downloads
 using Pkg
@@ -8,14 +8,14 @@ import Pkg: Registry
 using Tar
 using CoverageTools
 
-struct FsckFailure
+struct PkgFsckFailure
     local_path::String
     uuid::Base.UUID
     tree_hash::Base.SHA1
     has_build_file::Bool
 end
 
-function redownload(fail::FsckFailure)
+function redownload(fail::PkgFsckFailure)
     rm(fail.local_path; recursive=true, force=true)
     server = Pkg.pkg_server()
     url = "$server/package/$(fail.uuid)/$(fail.tree_hash)"
@@ -26,8 +26,8 @@ function redownload(fail::FsckFailure)
 end
 
 
-function fsck(; remove_cov_files::Bool=true, depot_path::Vector{String}=DEPOT_PATH)
-    failed_packages = FsckFailure[]
+function fsck_packages(; remove_cov_files::Bool=true, depot_path::Vector{String}=DEPOT_PATH)
+    failed_packages = PkgFsckFailure[]
     registries = Pkg.Registry.reachable_registries()
     for reg in registries
         for (_, pkg) in reg
@@ -37,7 +37,7 @@ function fsck(; remove_cov_files::Bool=true, depot_path::Vector{String}=DEPOT_PA
     return failed_packages
 end
 
-function fsck_package!(failed_packages::AbstractVector{FsckFailure}, pkg::Registry.PkgEntry;
+function fsck_package!(failed_packages::AbstractVector{PkgFsckFailure}, pkg::Registry.PkgEntry;
                        remove_cov_files::Bool, depot_path::Vector{String})
     pkginfo = Registry.registry_info(pkg)
     for (v, vinfo) in pkginfo.version_info
@@ -45,28 +45,61 @@ function fsck_package!(failed_packages::AbstractVector{FsckFailure}, pkg::Regist
         for slug in (Base.version_slug(pkg.uuid, tree_hash), Base.version_slug(pkg.uuid, tree_hash, 4))
             for depot in depot_path
                 path = joinpath(depot, "packages", pkg.name, slug)
-                if ispath(path)
-                    has_build_file = isdir(joinpath(path, "deps"))
-                    if Base.SHA1(Pkg.GitTools.tree_hash(path)) !== tree_hash
-                        if remove_cov_files
-                            try
-                                CoverageTools.clean_folder(path)
-                            catch
-                                @error "Coverage cleaning failed in $path"
-                            end
-                            # Cleaning cov files fixed it
-                            if Base.SHA1(Pkg.GitTools.tree_hash(path)) == tree_hash
-                                continue
-                            end
+                isdir(path) || continue
+
+                # Check package
+                has_build_file = isdir(joinpath(path, "deps"))
+                if Base.SHA1(Pkg.GitTools.tree_hash(path)) !== tree_hash
+                    if remove_cov_files
+                        try
+                            CoverageTools.clean_folder(path)
+                        catch
+                            @error "Coverage cleaning failed in $path"
                         end
-                        fail = FsckFailure(path, pkg.uuid, tree_hash, has_build_file)
-                        push!(failed_packages, fail)
+                        # Cleaning cov files fixed it
+                        if Base.SHA1(Pkg.GitTools.tree_hash(path)) == tree_hash
+                            continue
+                        end
                     end
+                    fail = PkgFsckFailure(path, pkg.uuid, tree_hash, has_build_file)
+                    push!(failed_packages, fail)
                 end
             end
         end
     end
     return failed_packages
+end
+
+
+struct ArtifactFsckFailure
+    local_path::String
+end
+
+function redownload(fail::ArtifactFsckFailure)
+    rm(fail.local_path; recursive=true, force=true)
+    server = Pkg.pkg_server()
+    hash = basename(fail.local_path)
+    url = "$server/artifact/$hash"
+    tmp = Downloads.download(url)
+    Tar.extract(`$(Pkg.PlatformEngines.exe7z()) x $tmp -so`, fail.local_path)
+    rm(tmp)
+    return
+end
+
+function fsck_artifacts()
+    failed_artifacts = ArtifactFsckFailure[]
+    for depot in DEPOT_PATH
+        artifacts_dir = joinpath(depot, "artifacts")
+        isdir(artifacts_dir) || continue
+        for sha in readdir(artifacts_dir)
+            artifact_dir = joinpath(artifacts_dir, sha)
+            isdir(artifact_dir) || continue
+            if  Base.SHA1(Pkg.GitTools.tree_hash(artifact_dir)) != Base.SHA1(sha)
+                push!(failed_artifacts, ArtifactFsckFailure(artifact_dir))
+            end
+        end
+    end
+    return failed_artifacts
 end
 
 end # module PkgFsck
